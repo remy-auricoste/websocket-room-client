54987134e04bda2c490a99cd1d2f233aee10bda2
remy <remy.auricoste@gmail.com>
2015-10-22 22:10:42 +0200

    remove useless test

--- src/test/js/specs/AdderSpec.js
+++
@@ -1,7 +0,0 @@
-/* import Adder */ var Adder = require('./../../../main/js/services/Adder');
-
-describe("Adder", function() {
-    it("should add", function() {
-        expect(Adder.add(3)).toBe(4);
-    });
-});
