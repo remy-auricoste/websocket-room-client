39faefdd34576358ff4af2cef329982069a50f68
remy <remy.auricoste@gmail.com>
2015-11-18 09:20:06 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var static = require('node-static');
-var SocketServer = require("rauricoste-websocket-room-server");
+var Server = require("rauricoste-websocket-room-server");
 
 var fileServer = new static.Server('.', {
     cache: 1
 });
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
-    }).resume();
-}).listen(8000);
+//require('http').createServer(function (request, response) {
+//    request.addListener('end', function () {
+//        fileServer.serve(request, response);
+//    }).resume();
+//}).listen(8000);
 
-//new SocketServer(8001);
+new Server.XhrServer(8001);
