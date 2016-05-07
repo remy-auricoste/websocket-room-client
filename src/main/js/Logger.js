4455dde8450043ba7d97f3fc1839e273bba13db5
remy <remy.auricoste@gmail.com>
2016-05-07 23:46:41 +0200

    change tests to rauricoste-logger

--- src/main/js/Logger.js
+++ src/main/js/Logger.js
@@ -1,4 +1,4 @@
-var rootLogger = require("loglevel").getLogger("rauricoste-websocket-room-client");
+var rootLogger = require("rauricoste-logger").getLogger("rauricoste-websocket-room-client");
 rootLogger.setLevel("warn");
 
 module.exports = rootLogger;
